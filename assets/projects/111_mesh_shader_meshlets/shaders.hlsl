b32b6eade4d290dfe7dda535cdd93c756d2c8015
Hai Nguyen <379079+chaoticbob@users.noreply.github.com>
2024-01-07 23:15:39 -0500

    Added 111_mesh_shader_meshlets and 112_mesh_shader_amplification

---
+++ assets/projects/111_mesh_shader_meshlets/shaders.hlsl
@@ -0,0 +1,64 @@
+struct CameraProperties {
+    float4x4 MVP;
+};
+
+ConstantBuffer<CameraProperties> Cam : register(b0);
+
+struct Vertex {
+    float3 Position;
+};
+
+struct Meshlet {
+	uint VertexOffset;
+	uint TriangleOffset;
+	uint VertexCount;
+	uint TriangleCount;
+};
+
+StructuredBuffer<Vertex>  Vertices        : register(t1);
+StructuredBuffer<Meshlet> Meshlets        : register(t2);
+StructuredBuffer<uint>    VertexIndices   : register(t3);
+StructuredBuffer<uint>    TriangleIndices : register(t4);
+
+struct MeshOutput {
+    float4 Position : SV_POSITION;
+    float3 Color    : COLOR;
+};
+
+[outputtopology("triangle")]
+[numthreads(128, 1, 1)]
+void msmain(
+                 uint       gtid : SV_GroupThreadID, 
+                 uint       gid  : SV_GroupID, 
+    out indices  uint3      triangles[126], 
+    out vertices MeshOutput vertices[64]) 
+{
+    Meshlet m = Meshlets[gid];
+    SetMeshOutputCounts(m.VertexCount, m.TriangleCount);
+       
+    if (gtid < m.TriangleCount) {
+        uint packed = TriangleIndices[m.TriangleOffset/4 + gtid];
+        uint vIdx0  = (packed >>  0) & 0xFF;
+        uint vIdx1  = (packed >>  8) & 0xFF;
+        uint vIdx2  = (packed >> 16) & 0xFF;
+        triangles[gtid] = uint3(vIdx0, vIdx1, vIdx2);
+    }
+
+    if (gtid < m.VertexCount) {
+        uint vertexIndex = m.VertexOffset + gtid;        
+        vertexIndex = VertexIndices[vertexIndex];
+
+        vertices[gtid].Position = mul(Cam.MVP, float4(Vertices[vertexIndex].Position, 1.0));
+        
+        float3 color = float3(
+            float(gid & 1),
+            float(gid & 3) / 4,
+            float(gid & 7) / 8);
+        vertices[gtid].Color = color;
+    }
+}
+
+float4 psmain(MeshOutput input) : SV_TARGET
+{
+    return float4(input.Color, 1);
+}
---
+++ assets/projects/112_mesh_shader_amplification/shaders.hlsl
@@ -0,0 +1,96 @@
+
+#define AS_GROUP_SIZE 32
+
+struct CameraProperties {
+    float4x4 MVP;
+};
+
+ConstantBuffer<CameraProperties> Cam : register(b0);
+
+struct Vertex {
+    float3 Position;
+};
+
+struct Meshlet {
+	uint VertexOffset;
+	uint TriangleOffset;
+	uint VertexCount;
+	uint TriangleCount;
+};
+
+StructuredBuffer<Vertex>  Vertices        : register(t1);
+StructuredBuffer<Meshlet> Meshlets        : register(t2);
+StructuredBuffer<uint>    VertexIndices   : register(t3);
+StructuredBuffer<uint>    TriangleIndices : register(t4);
+
+struct MeshOutput {
+    float4 Position : SV_POSITION;
+    float3 Color    : COLOR;
+};
+
+struct Payload {
+    uint MeshletIndex;
+};
+
+groupshared Payload sPayload;
+
+// -------------------------------------------------------------------------------------------------
+// Amplification Shader
+// -------------------------------------------------------------------------------------------------
+[numthreads(AS_GROUP_SIZE, 1, 1)]
+void asmain(
+    uint gtid : SV_GroupThreadID,
+    uint dtid : SV_DispatchThreadID,
+    uint gid  : SV_GroupID
+)
+{
+    sPayload.MeshletIndex = gid;
+    DispatchMesh(1, 1, 1, sPayload);
+}
+
+// -------------------------------------------------------------------------------------------------
+// Mesh Shader
+// -------------------------------------------------------------------------------------------------
+[outputtopology("triangle")]
+[numthreads(128, 1, 1)]
+void msmain(
+                 uint       gtid : SV_GroupThreadID, 
+                 uint       gid  : SV_GroupID, 
+     in payload  Payload    payload, 
+    out indices  uint3      triangles[126], 
+    out vertices MeshOutput vertices[64]) 
+{
+    uint meshletIndex = payload.MeshletIndex;
+
+    Meshlet m = Meshlets[meshletIndex];
+    SetMeshOutputCounts(m.VertexCount, m.TriangleCount);
+       
+    if (gtid < m.TriangleCount) {
+        uint packed = TriangleIndices[m.TriangleOffset/4 + gtid];
+        uint vIdx0  = (packed >>  0) & 0xFF;
+        uint vIdx1  = (packed >>  8) & 0xFF;
+        uint vIdx2  = (packed >> 16) & 0xFF;
+        triangles[gtid] = uint3(vIdx0, vIdx1, vIdx2);
+    }
+
+    if (gtid < m.VertexCount) {
+        uint vertexIndex = m.VertexOffset + gtid;        
+        vertexIndex = VertexIndices[vertexIndex];
+
+        vertices[gtid].Position = mul(Cam.MVP, float4(Vertices[vertexIndex].Position, 1.0));
+        
+        float3 color = float3(
+            float(meshletIndex & 1),
+            float(meshletIndex & 3) / 4,
+            float(meshletIndex & 7) / 8);
+        vertices[gtid].Color = color;
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+// Pixel Shader
+// -------------------------------------------------------------------------------------------------
+float4 psmain(MeshOutput input) : SV_TARGET
+{
+    return float4(input.Color, 1);
+}
